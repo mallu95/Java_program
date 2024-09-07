6aaac065810da9d6ffc4f911476e3e40f5da3260
mallu95 <mallikarjunkuntoji95@gmail.com>
2024-09-07 13:22:01 +0530

    add  files

---
+++ src/September/Lab002.java
@@ -0,0 +1,12 @@
+package September;
+
+public class Lab002 {
+    public static void main(String[] args){
+        int fact=1;
+        int n=5;
+        for(int i=n;i>=1;i--){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
