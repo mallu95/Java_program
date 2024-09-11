dc9527e91051bd508bf207df7d60f27687a429fc
mallu95 <mallikarjunkuntoji95@gmail.com>
2024-09-11 21:00:59 +0530

    Java

---
+++ src/September/Lab003.java
@@ -0,0 +1,18 @@
+package September;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //Data Type
+        short age=29;
+        String name="Mallikarjun";
+        int salary=27700;
+        boolean a=true;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(salary);
+        System.out.println(a);
+
+
+    }
+
+}
---
+++ src/ex_04_09_2024/Hello.java
@@ -0,0 +1,7 @@
+package ex_04_09_2024;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
