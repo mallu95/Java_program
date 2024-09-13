393dc03d4ba076202e7263253505a1f6c417738d
mallu95 <mallikarjunkuntoji95@gmail.com>
2024-09-13 21:14:23 +0530

    ex_11_09_2024

---
+++ src/ex_11_09_2024/Lap0009.java
@@ -0,0 +1,18 @@
+package ex_11_09_2024;
+
+public class Lap0009 {
+    public static void main(String[] args) {
+        //My details
+
+        String name="Mallikarjun";
+        byte age=29;
+        double sal=27700.60;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(sal);
+
+
+
+
+    }
+}
